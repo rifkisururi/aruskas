8d1c69fa6571ddf21b304ec5eb41505ca43323ea
rifki ahmad sururi <rifkisururi27@gmail.com>
2021-05-17 21:41:59 +0700

    perubahan logo

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -29,12 +29,14 @@
         <ul class="navbar-nav bg-gradient-dark sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
+
+            <div class="sidebar-brand-text mx-2"><img src="{{asset('logo/logo.png')}}" width="100%"></div>
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
-                <div class="sidebar-brand-icon rotate-n-0">
-                    <img src="{{asset('logo/logo.png')}}" width="60">
-                </div>
+
                 <div class="sidebar-brand-text mx-2">Pengelolaan Kas</div>
+
             </a>
+
 
             <!-- Divider -->
             <hr class="sidebar-divider my-0">
